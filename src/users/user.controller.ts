63e28e42a551f7c27f9f411ab44ea4c819162a8d
Zlaam <mrzalaam@gmail.com>
2024-04-11 20:40:06 +0500

    build(deps): update package-lock.json, package.json and user.controller.ts hashed the password

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,21 +2,28 @@
 import { type NextFunction, type Request, type Response } from "express";
 import createHttpError from "http-errors";
 import { User } from "./user.model.ts";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) {
-    const error = createHttpError(400, "All feilds are required");
-    return next(error);
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      const error = createHttpError(400, "All feilds are required");
+      return next(error);
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User Already exist with same email.");
+      return next(error);
+    }
+    res.status(200).json({
+      message: "User Registered Successfully",
+    });
+    // Hash password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+  } catch (error: any) {
+    console.log("Registeration failed :: ", error.message);
+    throw new Error("Registeration failed::something went wrong");
   }
-  const user = await User.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User Already exist with same email.");
-    return next(error);
-  }
-  res.status(400).json({
-    message: "User Registered Successfully",
-  });
 };
 export { createUser };
