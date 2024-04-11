3144c86bc8dbd081348c23a3510b36f1dafc093c
Zlaam <mrzalaam@gmail.com>
2024-04-11 15:57:03 +0500

    update 4 files and create 1 file

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Hello there");
+import app from "./src/app.ts";
+
+const startServer = () => {
+  const port = process.env.PORT || 5173;
+
+  app.listen(port, () => {
+    console.log(
+      `Server is listening on port:- *** http://localhost:${port}/  ***`
+    );
+  });
+};
+startServer();
