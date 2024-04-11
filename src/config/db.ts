571dfd4807d9dee274a10c246c4465abf4e20da5
Zlaam <mrzalaam@gmail.com>
2024-04-11 17:53:38 +0500

    update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-import { config } from "./config";
+import { config } from "./config.ts";
 const connectDB = async (): Promise<void> => {
   try {
     await mongoose.connect(config.connectionString);
   } catch (error: any) {
     console.log(`Error While connecting to the database::${error.message}`);
+    process.exit();
   }
 };
 export default connectDB;
