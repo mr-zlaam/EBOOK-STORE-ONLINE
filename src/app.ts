3a3d86f2e9b88a2cc5603b423d3bfabdb5d1ff25
Zlaam <mrzalaam@gmail.com>
2024-04-11 19:26:11 +0500

    "Moved global error handler to separate middleware

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,11 @@
-import express, {
-  type Request,
-  type Response,
-  type NextFunction,
-} from "express";
-import type { HttpError } from "http-errors";
-import { config } from "./config/config.ts";
+import express from "express";
+import GlobalErrHandler from "./middlewares/GlobalErrHandler.middleware.ts";
 const app = express();
 app.get("/", (req, res) => {
   res.json({
     message: "Hello world",
   });
 });
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const statuscode = err.statusCode || 500;
-  return res.status(statuscode).json({
-    message: err.message,
-    errorStack: config.env == "dev" ? err.stack : "",
-  });
-});
+//Gloabl handler
+app.use(GlobalErrHandler);
 export default app;
---
+++ src/middlewares/GlobalErrHandler.middleware.ts
@@ -0,0 +1,18 @@
+import { type Request, type Response, type NextFunction } from "express";
+import type { HttpError } from "http-errors";
+import { config } from "../config/config.ts";
+
+//imports end here
+const GlobalErrHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statuscode = err.statusCode || 500;
+  return res.status(statuscode).json({
+    message: err.message,
+    errorStack: config.env == "dev" ? err.stack : "",
+  });
+};
+export default GlobalErrHandler;
