739f78dfb822a4cad41aebacf8ceeecc8f8c5c31
Zlaam <mrzalaam@gmail.com>
2024-04-17 20:19:37 +0500

    add: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,20 @@ import GlobalErrHandler from "./middlewares/GlobalErrHandler.middleware.ts";
 import userRouter from "./users/userRouter.ts";
 import bookRouter from "./book/bookRouter.ts";
 const app = express();
+import cors from "cors";
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
+const whiteList = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 //Gloabl handler
 app.use(GlobalErrHandler);
 app.use("/api/users", userRouter);
